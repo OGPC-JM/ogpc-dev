3a8fe19ae6e0fb3e3e7a0141107a6c19976ad2bd
Mateo A <ogpcjam@gmail.com>
2024-01-13 12:51:11 -0800

    :) testing print

--- main.py
+++ main.py
@@ -5,11 +5,20 @@ playerhealth = 100
 weapondmg = 5
 allouttime = 15
 healthposion = 10
+def gamesetup():
+    print("Welcome to Jumanji!\n")
+    print("      _ _    _ __  __          _   _      _ _____")
+    print("     | | |  | |  \/  |   /\   | \ | |    | |_   _|")
+    print("     | | |  | | \  / |  /  \  |  \| |    | | | | ")
+    print("     | | |  | | |\/| | / /\ \ | . ` |_   | | | | ")
+    print("| |__| | |__| | |  | |/ ____ \| |\  | |__| |_| |")
+    print(" \____/ \____/|_|  |_/_/    \_\_| \_|\____/|_____|")
 def monsterBattle(monsterName, dmg, monsterhp):
     global weapondmg
     global playerhealth
     global allouttime
     global healthposion
+    attackdmgr = 0
     ma = True
     while ma == True:
         if monsterhp <= 0:
@@ -23,13 +32,14 @@ def monsterBattle(monsterName, dmg, monsterhp):
         print("[R]egular attack")
         print("[D]efence and counter(will do significantly less damage but enemy damage is also reduced.)")
         print("[U]se health potion")
+        print("[E]xit the game")
         battle_do = input()
         if battle_do == "A" or battle_do == "a":
             if allouttime <= 0:
                 print("You go on an all out attack")
                 time.sleep(0.5)
                 miss = random.randint(1,10)
-                monsterdmg = monsterdmg * 1.5
+                monsterdmg = monsterdmg * 2
                 if miss <= 5:
                     print("You race after ",monsterName," and slice, cut, and go all fury at you but you missed!")
             else:
@@ -42,21 +52,26 @@ def monsterBattle(monsterName, dmg, monsterhp):
                 print("You do a regualr attack")
                 print("But You miss")
             else:
-                weapondmg = weapondmg * 0.5
+                attackdmgr = attackdmg * 1.5
                 print("You do a regualr attack")
-                print("You use your sowrd to slice him, and do ", weapondmg)
+                print("You use your sowrd to slice him, and do ", attackdmgr, "damage")
+                monsterhp = monsterhp - attackdmgr
         elif battle_do == "U" or battle_do == "u":
             if healthposion == 0:
-                print("you drink the health poision and gain 10 health")
+                print("you drink the health poision and gain 10 healrth")
                 playerhealth  = playerhealth + 10
                 healthposion = 11
             else:
                 print("sorry, wait ", healthposion, "more rounds to have the health posion")
-        
-        
+        elif battle_do == "E" or battle_do == "e":
+            print("Ok, thanks for playing")
+            ma = False
+
+#        elif battle_do == ""
         if allouttime <= 0:
             allouttime = allouttime - 1
         if healthposion <= 0:
             healthposion - 1
         time.sleep(1)
-monsterBattle("goblin", 3, 50)
+gamesetup()
+#monsterBattle("goblin", 3, 50)
